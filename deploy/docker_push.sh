888adbd48fd483dedf1bae1d088a567329a58353
Chris Bethune <cbethune@uncharted.software>
2017-08-17 22:08:13 -0400

    version bump

---
+++ deploy/docker_push.sh
@@ -0,0 +1,2 @@
+docker push docker.uncharted.software/distil-pipeline-server:0.3
+docker push docker.uncharted.software/distil-pipeline-server:latest
--- deploy/docker_run.sh
+++ deploy/docker_run.sh
@@ -3,4 +3,4 @@ docker run \
     --name distil-pipeline-server \
     --rm \
     -p 9500:9500 \
-    docker.uncharted.software/distil-pipeline-server:0.1
+    docker.uncharted.software/distil-pipeline-server

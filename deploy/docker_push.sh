6e4788b911576a8fd2e03bc8ace0496a31c45f35
cbethune <cbethune@uncharted.software>
2018-01-04 15:30:32 -0500

    updates image version

--- deploy/docker_push.sh
+++ deploy/docker_push.sh
@@ -1,2 +1,2 @@
-docker push docker.uncharted.software/distil-pipeline-server:0.8.0
+docker push docker.uncharted.software/distil-pipeline-server:0.10.0
 docker push docker.uncharted.software/distil-pipeline-server:latest

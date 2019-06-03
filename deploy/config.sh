1c5e8fee3d98e4fb05179e8342b2d6f4509180e9
cbethune <cbethune@uncharted.software>
2019-06-03 12:29:55 -0400

    version bump

--- deploy/config.sh
+++ deploy/config.sh
@@ -2,7 +2,7 @@
 
 # name and version of docker image that will be created
 DOCKER_IMAGE_NAME=distil-pipeline-server
-DOCKER_IMAGE_VERSION=0.20.0
+DOCKER_IMAGE_VERSION=0.20.1
 
 # name of docker repo for image
 DOCKER_REPO=docker.uncharted.software
